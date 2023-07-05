901821b3291f2248aeaa8978e426568935fbbd7d
stambe-sz <52385541+stambe-sz@users.noreply.github.com>
2023-07-05 23:57:37 +0300

    .

---
+++ ResellerAppLombok/src/main/java/com/example/resellerapplombok/ResellerAppLombokApplication.java
@@ -0,0 +1,13 @@
+package com.example.resellerapplombok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResellerAppLombokApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResellerAppLombokApplication.class, args);
+    }
+
+}
---
+++ ResellerAppLombok/src/test/java/com/example/resellerapplombok/ResellerAppLombokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.resellerapplombok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResellerAppLombokApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
