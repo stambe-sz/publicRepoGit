cb7eb2435b16708d47219b6f00fbc49eaf11f63d
stambe-sz <52385541+stambe-sz@users.noreply.github.com>
2023-07-07 00:54:40 +0300

    .

--- ResellerAppLombok/src/main/java/com/resellerapp/model/entity/BaseEntity.java
+++ ResellerAppLombok/src/main/java/com/resellerapp/model/entity/BaseEntity.java
@@ -1,24 +1,20 @@
 package com.resellerapp.model.entity;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 
+@Setter
+@Getter
+@NoArgsConstructor
 @MappedSuperclass
 public abstract class BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    public BaseEntity() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 }
---
+++ ResellerAppLombok/src/main/java/com/resellerapp/model/entity/User.java
@@ -0,0 +1,19 @@
+package com.resellerapp.model.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User extends BaseEntity{
+    @Column(unique = true,nullable = false)
+    private String username;
+}
