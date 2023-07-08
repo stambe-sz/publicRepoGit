746ed1d24534425690048326e8395184d933495c
stambe-sz <52385541+stambe-sz@users.noreply.github.com>
2023-07-08 21:29:17 +0300

    .

--- ResellerAppLombok/src/main/java/com/resellerapp/model/entity/Offer.java
+++ ResellerAppLombok/src/main/java/com/resellerapp/model/entity/Offer.java
@@ -3,7 +3,9 @@ package com.resellerapp.model.entity;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Getter
@@ -11,5 +13,15 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "offers")
 public class Offer extends BaseEntity{
+
+    @Column(nullable = false)
+    private String description;
+    @Column(nullable = false, columnDefinition = "FOAL")
+    private Double price;
+
+    @ManyToOne
+    private Condition condition;
+    @ManyToOne
+    private User user;
 
 }
--- ResellerAppLombok/src/main/java/com/resellerapp/model/entity/User.java
+++ ResellerAppLombok/src/main/java/com/resellerapp/model/entity/User.java
@@ -3,10 +3,14 @@ package com.resellerapp.model.entity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.w3c.dom.stylesheets.LinkStyle;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import java.util.List;
 
 @Setter
 @Getter
@@ -18,4 +22,10 @@ public class User extends BaseEntity{
     private String username;
     @Column(nullable = false)
     private String password;
+    @Email
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @OneToMany()
+    private List<Offer> offers;
 }
