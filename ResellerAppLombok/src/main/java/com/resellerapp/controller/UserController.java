ca620598cc723e785a439873b66ce2e4bc896d25
stambe-sz <52385541+stambe-sz@users.noreply.github.com>
2023-07-11 23:55:01 +0300

    .

--- ResellerAppLombok/src/main/java/com/resellerapp/controller/UserController.java
+++ ResellerAppLombok/src/main/java/com/resellerapp/controller/UserController.java
@@ -1,8 +1,14 @@
 package com.resellerapp.controller;
 
+import com.resellerapp.model.binding.UserRegisterBindingModel;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/users")
@@ -11,5 +17,12 @@ public class UserController {
     @GetMapping("/register")
     public String register(){
         return "register";
+    }
+
+    @PostMapping("/register")
+    public String registerConfirm(@Valid UserRegisterBindingModel userRegisterBindingModel,
+                                  BindingResult bindingResult, RedirectAttributes redirectAttributes){
+        
+
     }
 }
--- ResellerAppLombok/src/main/java/com/resellerapp/model/binding/UserRegisterBindingModel.java
+++ ResellerAppLombok/src/main/java/com/resellerapp/model/binding/UserRegisterBindingModel.java
@@ -4,12 +4,19 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Size;
+
 @Setter
 @Getter
 @NoArgsConstructor
 public class UserRegisterBindingModel {
+    @Size(min = 3,max = 20)
     private String username;
+    @Email
     private String email;
+    @Size(min = 3,max = 20)
     private String password;
+    @Size(min = 3,max = 20)
     private String confirmPassword;
 }
