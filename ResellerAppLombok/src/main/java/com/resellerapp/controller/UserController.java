530121e6fcd9f3072bda1bffca45c102673faa35
stambe-sz <52385541+stambe-sz@users.noreply.github.com>
2023-07-12 01:16:07 +0300

    .

--- ResellerAppLombok/src/main/java/com/resellerapp/controller/UserController.java
+++ ResellerAppLombok/src/main/java/com/resellerapp/controller/UserController.java
@@ -4,6 +4,7 @@ import com.resellerapp.model.binding.UserRegisterBindingModel;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -38,5 +39,15 @@ public class UserController {
 
         return "redirect:login";
 
+    }
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+    @ModelAttribute
+    public UserRegisterBindingModel userRegisterBindingModel(){
+        return new UserRegisterBindingModel();
     }
 }
