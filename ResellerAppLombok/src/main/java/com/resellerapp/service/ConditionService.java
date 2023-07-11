30f88beb7e72ae4acc8df54e46f3e8c3b654943c
stambe-sz <52385541+stambe-sz@users.noreply.github.com>
2023-07-11 13:27:48 +0300

    .

--- ResellerAppLombok/src/main/java/com/resellerapp/service/ConditionService.java
+++ ResellerAppLombok/src/main/java/com/resellerapp/service/ConditionService.java
@@ -1,8 +1,12 @@
 package com.resellerapp.service;
 
+import com.resellerapp.model.entity.Condition;
+import com.resellerapp.model.enums.ConditionNameEnum;
 import com.resellerapp.repository.ConditionRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
 
 @Service
 public class ConditionService {
@@ -19,6 +23,17 @@ public class ConditionService {
         if(conditionRepository.count() != 0){
             return;
         }
-        
+        Arrays.stream(ConditionNameEnum.values())
+                .forEach(conditionNameEnum -> {
+                    Condition condition = new Condition();
+                    condition.setConditionName(conditionNameEnum);
+                    switch (conditionNameEnum){
+                        case EXCELLENT -> condition.setDescription("In perfect condition");
+                        case GOOD -> condition.setDescription("Some signs of wear and tear or minor defects");
+                        case ACCEPTABLE -> condition.setDescription("The item is fairly worn but continues to function properly");
+                    }
+                    conditionRepository.save(condition);
+                });
+
     }
 }
