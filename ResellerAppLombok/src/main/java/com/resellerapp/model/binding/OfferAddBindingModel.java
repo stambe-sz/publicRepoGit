386e4f026f81dd773ef281f05f54d4252003c6d6
stambe-sz <52385541+stambe-sz@users.noreply.github.com>
2023-07-14 23:50:05 +0300

    .

---
+++ ResellerAppLombok/src/main/java/com/resellerapp/model/binding/OfferAddBindingModel.java
@@ -0,0 +1,9 @@
+package com.resellerapp.model.binding;
+
+import com.resellerapp.model.enums.ConditionNameEnum;
+
+public class OfferAddBindingModel {
+    private String description;
+    private Double price;
+    private ConditionNameEnum condition;
+}
