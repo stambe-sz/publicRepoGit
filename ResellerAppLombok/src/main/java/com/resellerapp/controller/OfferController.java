aa0cf2e5354add508e643c92f5b5f84cd634adba
stambe-sz <52385541+stambe-sz@users.noreply.github.com>
2023-07-15 11:35:26 +0300

    .

--- ResellerAppLombok/src/main/java/com/resellerapp/controller/OfferController.java
+++ ResellerAppLombok/src/main/java/com/resellerapp/controller/OfferController.java
@@ -1,8 +1,15 @@
 package com.resellerapp.controller;
 
+import com.resellerapp.model.binding.OfferAddBindingModel;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/offers")
@@ -11,5 +18,22 @@ public class OfferController {
     @GetMapping("/add")
     public String add(){
         return "offer-add";
+    }
+
+    @PostMapping("/add")
+    public String addConfirm(@Valid OfferAddBindingModel offerAddBindingModel,
+                             BindingResult bindingResult,
+                             RedirectAttributes redirectAttributes){
+        if (bindingResult.hasErrors()){
+
+        }
+
+        //add to db
+
+        return "redirect:/";
+    }
+
+    public OfferAddBindingModel offerAddBindingModel(){
+        return new OfferAddBindingModel();
     }
 }
--- ResellerAppLombok/src/main/java/com/resellerapp/model/binding/OfferAddBindingModel.java
+++ ResellerAppLombok/src/main/java/com/resellerapp/model/binding/OfferAddBindingModel.java
@@ -1,9 +1,25 @@
 package com.resellerapp.model.binding;
 
 import com.resellerapp.model.enums.ConditionNameEnum;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
+
+@Setter
+@Getter
+@NoArgsConstructor
 public class OfferAddBindingModel {
+    @NotNull
+    @Size(min = 2, max = 50,  message = "Description length must be between 2 and 50 characters!")
     private String description;
+    @NotNull
+    @Positive(message = "Price must be positive number!")
     private Double price;
+    @NotBlank(message = "You must select a condition!")
     private ConditionNameEnum condition;
 }
