b22834c3565b2ea78a3d45f29917735cea7f39c9
stambe-sz <52385541+stambe-sz@users.noreply.github.com>
2023-07-22 16:53:17 +0300

    .

---
+++ PlannerApp/src/main/java/com/plannerapp/model/entity/Priority.java
@@ -0,0 +1,18 @@
+package com.plannerapp.model.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@Entity
+@Table(name = "priorities")
+public class Priority extends BaseEntity{
+
+
+}
--- PlannerApp/src/main/java/com/plannerapp/model/entity/User.java
+++ PlannerApp/src/main/java/com/plannerapp/model/entity/User.java
@@ -17,4 +17,9 @@ public class User extends BaseEntity{
 
     @Column(nullable = false,unique = true)
     private String username;
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false,unique = true)
+    private String email;
+
 }
