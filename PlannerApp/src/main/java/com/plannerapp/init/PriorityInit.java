a71fd0903f0c36af4ff7af49dc9e0b31fc7578b8
stambe-sz <52385541+stambe-sz@users.noreply.github.com>
2023-07-23 23:24:03 +0300

    .

---
+++ PlannerApp/src/main/java/com/plannerapp/init/PriorityInit.java
@@ -0,0 +1,12 @@
+package com.plannerapp.init;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PriorityInit implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
