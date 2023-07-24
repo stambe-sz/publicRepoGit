2866925b928ae6195f5a909173bfa249f6d0e601
stambe-sz <52385541+stambe-sz@users.noreply.github.com>
2023-07-24 17:25:21 +0300

    .

--- PlannerApp/src/main/java/com/plannerapp/controller/UserController.java
+++ PlannerApp/src/main/java/com/plannerapp/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.plannerapp.controller;
 
+import com.plannerapp.model.binding.UserLoginBindingModel;
 import com.plannerapp.model.binding.UserRegisterBindingModel;
 import com.plannerapp.model.service.UserServiceModel;
 import com.plannerapp.service.UserService;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -52,5 +54,22 @@ public class UserController {
                 .map(userRegisterBindingModel, UserServiceModel.class));
 
         return "redirect:login";
+    }
+    @GetMapping("/login")
+    public String login(Model model){
+        if (!model.containsAttribute("isFound")){
+            model.addAttribute("isFound",true);
+
+        }
+        return "login";
+    }
+
+    @ModelAttribute
+    public UserRegisterBindingModel userRegisterBindingModel(){
+        return new UserRegisterBindingModel();
+    }
+    @ModelAttribute
+    public UserLoginBindingModel userLoginBindingModel(){
+        return new UserLoginBindingModel();
     }
 }
