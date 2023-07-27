06486af68f174aa375a38723d39a1e91dbeb9d42
stambe-sz <52385541+stambe-sz@users.noreply.github.com>
2023-07-27 17:18:11 +0300

    .

---
+++ PlannerApp/src/main/java/com/plannerapp/controller/HomeController.java
@@ -0,0 +1,22 @@
+package com.plannerapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.HttpMediaTypeException;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String index(HttpSession httpSession, Model model){
+        if (httpSession.getAttribute("id") == null){
+            return "index";
+        }
+
+
+        return "home";
+    }
+}
