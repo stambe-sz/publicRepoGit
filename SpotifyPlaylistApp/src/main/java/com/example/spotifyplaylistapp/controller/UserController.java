e4a7c081b4cf557b0176bc76850538dadd988417
stambe-sz <52385541+stambe-sz@users.noreply.github.com>
2023-08-07 17:10:21 +0300

    .

--- SpotifyPlaylistApp/src/main/java/com/example/spotifyplaylistapp/controller/UserController.java
+++ SpotifyPlaylistApp/src/main/java/com/example/spotifyplaylistapp/controller/UserController.java
@@ -1,11 +1,17 @@
 package com.example.spotifyplaylistapp.controller;
 
+import com.example.spotifyplaylistapp.model.binding.UserRegisterBindingModel;
 import com.example.spotifyplaylistapp.service.UserService;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/users")
@@ -24,7 +30,23 @@ public class UserController {
         return "register";
     }
     @PostMapping("/register")
-    public String confirmRegister(){
+    public String confirmRegister(@Valid UserRegisterBindingModel userRegisterBindingModel,
+                                  BindingResult bindingResult,
+                                  RedirectAttributes redirectAttributes){
+        if (bindingResult.hasErrors() || !userRegisterBindingModel.getPassword()
+                .equals(userRegisterBindingModel.getConfPassword())){
+            redirectAttributes.addFlashAttribute("userRegisterBindingModel",userRegisterBindingModel);
+            redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.userRegisterBindingModel", bindingResult);
 
+            return "redirect:register";
+        }
+
+
+        return "redirect:/login";
+    }
+
+    @ModelAttribute
+    public UserRegisterBindingModel userRegisterBindingModel(){
+        return new UserRegisterBindingModel();
     }
 }
--- SpotifyPlaylistApp/src/main/java/com/example/spotifyplaylistapp/model/binding/UserRegisterBindingModel.java
+++ SpotifyPlaylistApp/src/main/java/com/example/spotifyplaylistapp/model/binding/UserRegisterBindingModel.java
@@ -4,13 +4,20 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Size;
+
 @Getter
 @Setter
 @NoArgsConstructor
 public class UserRegisterBindingModel {
 
+    @Size(min = 3,max = 20,message = "Username length must be between 3 and 20 characters")
     private String username;
+    @Email(message = "Email cannot by empty")
     private String email;
+    @Size(min = 3,max = 20,message = "Password length must be between 3 and 20 characters")
     private String password;
+    @Size(min = 3,max = 20,message = "")
     private String confPassword;
 }
