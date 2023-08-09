50ed3b4e3a4957050178adb208deb77d2f283e9e
stambe-sz <52385541+stambe-sz@users.noreply.github.com>
2023-08-09 22:34:43 +0300

    .

---
+++ SpotifyPlaylistApp/src/main/java/com/example/spotifyplaylistapp/controller/HomeController.java
@@ -0,0 +1,19 @@
+package com.example.spotifyplaylistapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String index(HttpSession httpSession, Model model) {
+        if (httpSession.getAttribute("id") == null) {
+            return "index";
+        }
+        return "home";
+    }
+}
--- SpotifyPlaylistApp/src/main/java/com/example/spotifyplaylistapp/model/binding/UserRegisterBindingModel.java
+++ SpotifyPlaylistApp/src/main/java/com/example/spotifyplaylistapp/model/binding/UserRegisterBindingModel.java
@@ -5,6 +5,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 @Getter
@@ -14,6 +15,7 @@ public class UserRegisterBindingModel {
 
     @Size(min = 3,max = 20,message = "Username length must be between 3 and 20 characters")
     private String username;
+    @NotBlank
     @Email(message = "Email cannot by empty")
     private String email;
     @Size(min = 3,max = 20,message = "Password length must be between 3 and 20 characters")
