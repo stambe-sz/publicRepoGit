4a2b572d6c1f1375b1150f57caa9c272ea52cf9c
stambe-sz <52385541+stambe-sz@users.noreply.github.com>
2023-08-10 00:48:57 +0300

    .

--- SpotifyPlaylistApp/src/main/java/com/example/spotifyplaylistapp/controller/UserController.java
+++ SpotifyPlaylistApp/src/main/java/com/example/spotifyplaylistapp/controller/UserController.java
@@ -97,4 +97,8 @@ public class UserController {
     public UserRegisterBindingModel userRegisterBindingModel() {
         return new UserRegisterBindingModel();
     }
+    @ModelAttribute
+    public UserLoginBindingModel userLoginBindingModel(){
+        return new UserLoginBindingModel();
+    }
 }
