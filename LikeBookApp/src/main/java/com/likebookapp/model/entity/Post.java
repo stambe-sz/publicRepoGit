e9e51ade791fa811d4d55a44885563bbe424959f
stambe-sz <52385541+stambe-sz@users.noreply.github.com>
2023-09-16 02:09:43 +0300

    n

---
+++ LikeBookApp/src/main/java/com/likebookapp/model/entity/Post.java
@@ -0,0 +1,25 @@
+package com.likebookapp.model.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@Entity
+@Table(name = "posts")
+public class Post extends BaseEntity{
+
+    @Column(nullable = false)
+    private String content;
+    @ManyToOne
+    @NotNull
+    private User user;
+    @ManyToMany(mappedBy = "likePosts")
+    private List<User> userLikes;
+}
--- LikeBookApp/src/main/java/com/likebookapp/model/entity/User.java
+++ LikeBookApp/src/main/java/com/likebookapp/model/entity/User.java
@@ -5,6 +5,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.List;
 
 
 @Setter
@@ -19,4 +20,9 @@ public class User extends BaseEntity{
     private String password;
     @Column(nullable = false,unique = true)
     private String email;
+    @OneToMany
+    private List<Post> posts;
+
+    @ManyToMany
+    private List<Post> likePosts;
 }
