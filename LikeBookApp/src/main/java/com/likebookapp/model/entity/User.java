27c3eb8117d79f773574f79b2e91bfb9152c5417
stambe-sz <52385541+stambe-sz@users.noreply.github.com>
2023-09-15 21:07:08 +0300

    j

--- LikeBookApp/src/main/java/com/likebookapp/model/entity/User.java
+++ LikeBookApp/src/main/java/com/likebookapp/model/entity/User.java
@@ -1,4 +1,22 @@
 package com.likebookapp.model.entity;
 
-public class User {
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+
+@Setter
+@Getter
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User extends BaseEntity{
+    @Column(nullable = false,unique = true)
+    private String username;
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false,unique = true)
+    private String email;
 }
