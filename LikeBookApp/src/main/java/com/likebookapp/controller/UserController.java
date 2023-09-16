f99bee6b0d88cb79af4dc8f987d2fb71aed8030f
stambe-sz <52385541+stambe-sz@users.noreply.github.com>
2023-09-16 22:11:34 +0300

    p

---
+++ LikeBookApp/src/main/java/com/likebookapp/controller/UserController.java
@@ -0,0 +1,36 @@
+package com.likebookapp.controller;
+
+import com.likebookapp.service.UserService;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+@RequestMapping("/users")
+public class UserController {
+    private final UserService userService;
+    private final ModelMapper modelMapper;
+    private final HttpSession httpSession;
+
+    public UserController(UserService userService, ModelMapper modelMapper, HttpSession httpSession) {
+        this.userService = userService;
+        this.modelMapper = modelMapper;
+        this.httpSession = httpSession;
+    }
+    @GetMapping("/register")
+    public String register(){
+        return "register";
+    }
+
+    @GetMapping("/login")
+    public String login(Model model){
+        if (!model.containsAttribute("isFound")) {
+            model.addAttribute("isFound", true);
+        }
+        return "login";
+    }
+}
